f8ece8b21c33d9a9402f4fccf31b2e95300f0347
Rajat Thapa <rajatthapa826@gmail.com>
2026-02-07 10:27:34 +1100

    changed the setting for next js

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,13 @@ const nextConfig = {
 
     return webpackConfig
   },
+
+  output: 'standalone',
+  swcMinify: true,
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
